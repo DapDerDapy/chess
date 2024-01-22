a19f97233d300e79929a5233d20842c05f271061
Daniel Phillips <dapderdapy@gmail.com>
2024-01-22 12:33:37 -0700

    board tests pass

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -41,18 +43,116 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        int newRow;
-        int newCol;
+
+        for(int i = 0; i <= 8; i++){
+            for (int j = 0; j <= 8; j++){
+                grid[i][j] = null;
+            }
+        }
 
         ChessPosition newPosition;
-
+        ChessPiece newPiece;
 
         // Add pawns to white
         for (int i = 1; i <= 8; i++){
             newPosition = new ChessPosition(2, i);
-            //addPiece(newPosition, PieceType.PAWN);
+            newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+            addPiece(newPosition, newPiece);
         }
 
+        // Add pawns to black
+        for (int i = 1; i <= 8; i++){
+            newPosition = new ChessPosition(7, i);
+            newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
+            addPiece(newPosition, newPiece);
+        }
 
+        // Add White Rooks
+        newPosition = new ChessPosition(1,1);
+        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        addPiece(newPosition, newPiece);
+
+        newPosition = new ChessPosition(1,8);
+        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        addPiece(newPosition, newPiece);
+
+        // Add Black Rooks
+        newPosition = new ChessPosition(8,1);
+        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        addPiece(newPosition, newPiece);
+
+        newPosition = new ChessPosition(8,8);
+        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        addPiece(newPosition, newPiece);
+
+        // Add White Knights
+        newPosition = new ChessPosition(1,2);
+        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        addPiece(newPosition, newPiece);
+
+        newPosition = new ChessPosition(1,7);
+        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+        addPiece(newPosition, newPiece);
+
+        // Add Black Knights
+        newPosition = new ChessPosition(8,2);
+        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        addPiece(newPosition, newPiece);
+
+        newPosition = new ChessPosition(8,7);
+        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+        addPiece(newPosition, newPiece);
+
+
+        // Add White Bishops
+        newPosition = new ChessPosition(1,3);
+        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        addPiece(newPosition, newPiece);
+
+        newPosition = new ChessPosition(1,6);
+        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+        addPiece(newPosition, newPiece);
+
+        // Add Black Bishops
+        newPosition = new ChessPosition(8,3);
+        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        addPiece(newPosition, newPiece);
+
+        newPosition = new ChessPosition(8,6);
+        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+        addPiece(newPosition, newPiece);
+
+
+        // Add White Queen
+        newPosition = new ChessPosition(1,4);
+        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+        addPiece(newPosition, newPiece);
+
+        // Add Black Queen
+        newPosition = new ChessPosition(8,4);
+        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+        addPiece(newPosition, newPiece);
+
+        // Add White King
+        newPosition = new ChessPosition(1,5);
+        newPiece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        addPiece(newPosition, newPiece);
+
+        // Add Black King
+        newPosition = new ChessPosition(8,5);
+        newPiece = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+        addPiece(newPosition, newPiece);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ChessBoard that)) return false;
+        return Arrays.deepEquals(grid, that.grid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(grid);
     }
 }
