c3f1efc9ee79b0c6f0dc2e3262072d4b07e23310
Daniel Phillips <dapderdapy@gmail.com>
2024-01-31 14:17:31 -0700

    passes 1 test

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -11,10 +12,10 @@ import java.util.Collection;
 public class ChessGame {
 
     private TeamColor teamTurn;
+    private ChessBoard grid;
 
     public ChessGame() {
         this.teamTurn = TeamColor.WHITE;
-
     }
 
     /**
@@ -49,7 +50,89 @@ public class ChessGame {
      * startPosition
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-        throw new RuntimeException("Not implemented");
+
+        // If there is nothing at the start position, return null
+        Collection<ChessMove> moves = new HashSet<>();
+
+        if (startPosition == null){
+            return null;
+        }
+
+        // Intialize parameter startPosition
+        ChessPiece piece = grid.getPiece(startPosition);
+        Collection<ChessMove> potentialMoves = piece.pieceMoves(grid, startPosition);
+
+        // Filter out invalid moves
+        for (ChessMove move : potentialMoves) {
+            // Perform the check here. At a single move with the given start position and grid, would that move
+            // put the king in danger? However, if the piece can move into a position where it can destroy the
+            // piece that endangers the king, that is okay. But if there's another piece that can get the king
+            // then it cannot go there.
+
+            if (isMoveValid(move, grid, startPosition)) {
+                moves.add(move);
+            }
+        }
+        return moves;
+    }
+
+    private boolean isMoveValid(ChessMove move, ChessBoard board, ChessPosition startPosition){
+
+        // Say, the bishop moves. Every possible place where it thinks it can move, it should check every single
+        // other enemy piece and see if it can attack the king, if it can, that move is NOT valid!
+
+        // maybe it should just see if any piece can hit the king of that piece moves at all.
+
+        // It should check the moves from every potential move from every piece.
+
+        Collection<ChessMove> opponentMoves = new HashSet<>();
+
+        ChessPosition startPiecePosition = new ChessPosition(startPosition.getRow(), startPosition.getColumn());
+        ChessPiece startPiece = board.getPiece(startPiecePosition);
+
+        // hypothetically.... say the piece moved from its position?
+        board.addPiece(startPosition, null);
+
+
+        for (int i = 1; i <= 8; i++){
+            for (int j = 1; j <= 8; j++){
+
+                // Check this piece, is it a
+                ChessPosition opponentPosition = new ChessPosition(i,j);
+                ChessPiece opponentPiece = board.getPiece(opponentPosition);
+
+                if (opponentPiece != null){
+                    if (opponentPiece.getTeamColor() != startPiece.getTeamColor()){
+
+                        //get all possible moves from that piece if that piece moved from its position
+
+                        opponentMoves = opponentPiece.pieceMoves(board, new ChessPosition(i,j));
+
+                        // 1st, check if any of those moves INCLUDE the king's position
+                        // IF SO, return FALSE AFTER STEP three
+
+                        // 2nd, check if the chess piece that WOULD theoretically check the king
+                        // can take the opponent piece
+
+                        if (move.getEndPosition() == opponentPosition){
+
+
+
+                            // 3rd, check if taking that opponent piece STILL leaves the king exposed
+                            // IF SO, return false
+                        }
+
+
+                        // return
+                    }
+                }
+            }
+        }
+
+
+
+
+        return true;
     }
 
     /**
@@ -69,7 +152,10 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+
+        // if, on the opponents turn, the king can be captured.
+
+        return false;
     }
 
     /**
@@ -79,7 +165,10 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+
+        //If King Valid moves == NULL
+
+        return false;
     }
 
     /**
@@ -90,7 +179,10 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        //if valid moves == NONE
+
+
+        return false;
     }
 
     /**
@@ -99,7 +191,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        grid = board;
     }
 
     /**
@@ -108,6 +200,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return grid;
     }
 }
